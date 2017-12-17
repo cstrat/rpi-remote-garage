5dd59a9c7d5167dcdb215db93a8af6062adc4362
Chris Stratford <neester@gmail.com>
2017-12-17 21:02:28 +1100

    Update IP Filter

--- ipfilter.js
+++ ipfilter.js
@@ -3,7 +3,12 @@
 */
 
 const ipFilter = function(req, res, next) {
-  if (['::1', '::ffff:10.0.0.22', '10.0.0.22'].indexOf(req.ip) > -1) {
+  if ([ '::1',
+        '::ffff:10.0.0.20', '10.0.0.20',    // Macbook
+        '::ffff:10.0.0.22', '10.0.0.22',    // iPhone
+        '::ffff:10.0.0.23', '10.0.0.23',    // iPhone
+        '::ffff:10.10.10.10', '10.10.10.10' // rPi Server
+      ].indexOf(req.ip) > -1) {
     next();
   }else{
     res.status(403).json({ error: 'forbidden'}).end();
